ef5e2220f6d43f5561887319f5dbe36372d6bd26
Mtzallqmy <mtzallqmy@gmail.com>
2026-07-15 03:35:02 +0300

    Type public auth redirect environment variables

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -7,6 +7,8 @@ interface ImportMetaEnv {
   readonly NEXT_PUBLIC_SUPABASE_URL?: string
   readonly NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY?: string
   readonly NEXT_PUBLIC_SUPABASE_ANON_KEY?: string
+  readonly VITE_APP_URL?: string
+  readonly NEXT_PUBLIC_APP_URL?: string
 }
 
 interface ImportMeta {
